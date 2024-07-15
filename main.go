ed9e233642172f4212c5f647f791d223e99697d5
Data Dius <16731794+datadius@users.noreply.github.com>
2024-07-15 12:31:54 +0200

    hello world of discord bots

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var botSession *discordgo.Session
+
+func init() {
+	var botToken string
+	var err error
+	botToken = os.Getenv("discord_bot_token")
+
+	botSession, err = discordgo.New("Bot " + botToken)
+	if err != nil {
+		log.Fatalf("Invalid bot token: %v", err)
+	}
+}
+
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "hello",
+			Description: "A command to say back hello to the user",
+		},
+	}
+
+	commandHandlers = map[string]func(botSession *discordgo.Session, botInteraction *discordgo.InteractionCreate){
+		"hello": func(botSession *discordgo.Session, botInteraction *discordgo.InteractionCreate) {
+			botSession.InteractionRespond(
+				botInteraction.Interaction,
+				&discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Hey there! Congratulations, your first command worked",
+					},
+				})
+		},
+	}
+)
+
+func init() {
+	botSession.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+	botSession.AddHandler(func(botSession *discordgo.Session, botReady *discordgo.Ready) {
+		log.Printf(
+			"Logged in as: %v#%v",
+			botSession.State.User.Username,
+			botSession.State.User.Discriminator,
+		)
+	})
+	err := botSession.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	log.Printf("Adding %d commands...\n", len(commands))
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := botSession.ApplicationCommandCreate(
+			botSession.State.User.ID,
+			"",
+			v,
+		)
+		log.Printf("Created %v command and registered", v.Name)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer botSession.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	log.Println("Removing commands...")
+
+	for _, v := range registeredCommands {
+		err := botSession.ApplicationCommandDelete(
+			botSession.State.User.ID,
+			"",
+			v.ID,
+		)
+		if err != nil {
+			log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+		}
+	}
+
+	log.Println("Gracefully shutting down.")
+}
