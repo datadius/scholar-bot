7c11c28604220b3050d7b17363cccc20915f0790
Data Dius <16731794+datadius@users.noreply.github.com>
2024-07-16 21:35:36 +0200

    added top 10 google scholar result

--- apihandlers/main.go
+++ apihandlers/main.go
@@ -9,14 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type StudyEmbed struct {
+type StudyStruct struct {
 	Title    string
 	Url      string
 	Authors  string
 	Abstract string
 }
 
-func QueryFirstGs(query string) (*StudyEmbed, bool) {
+func QueryFirstGs(query string) (*StudyStruct, bool) {
 	// Define the URL of the Google Scholar search page
 	urlQuery := fmt.Sprintf(
 		"https://scholar.google.com/scholar?hl=en&q=%s",
@@ -72,7 +72,74 @@ func QueryFirstGs(query string) (*StudyEmbed, bool) {
 		abstractElem := s.Find("div.gs_rs")
 		abstract := abstractElem.Text()
 
-		return &StudyEmbed{Title: title, Url: urlResp, Authors: authors, Abstract: abstract}, true
+		return &StudyStruct{Title: title, Url: urlResp, Authors: authors, Abstract: abstract}, true
+	} else {
+		fmt.Println("Failed to retrieve the page. Status code:", resp.StatusCode)
+		return nil, false
+	}
+}
+
+func QueryTopTenGs(query string) (*[]StudyStruct, bool) {
+	// Define the URL of the Google Scholar search page
+	urlQuery := fmt.Sprintf(
+		"https://scholar.google.com/scholar?hl=en&q=%s",
+		url.QueryEscape(query),
+	)
+	fmt.Println(urlQuery)
+
+	// Define a User-Agent header
+	headers := map[string]string{
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+	}
+
+	// Send a GET request to the URL with the User-Agent header
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", urlQuery, nil)
+	if err != nil {
+		log.Printf("error getting the request %v", err)
+		return nil, false
+	}
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	// Check if the request was successful (status code 200)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("error in executing the request %v", err)
+		return nil, false
+	}
+	defer resp.Body.Close()
+	log.Println(resp.StatusCode)
+
+	if resp.StatusCode == 200 {
+		// Parse the HTML content of the page using goquery
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Printf("error in parsing html %v", err)
+			return nil, false
+		}
+
+		var studySlice []StudyStruct
+
+		// Find all the search result blocks with class "gs_ri"
+		doc.Find(".gs_ri").Each(func(i int, s *goquery.Selection) {
+			// Extract the title and URL
+			titleElem := s.Find("h3.gs_rt")
+			title := titleElem.Text()
+			urlResp, _ := titleElem.Find("a").Attr("href")
+
+			studySlice = append(
+				studySlice,
+				StudyStruct{Title: title, Url: urlResp, Authors: "", Abstract: ""},
+			)
+
+		})
+		if len(studySlice) != 0 {
+			return &studySlice, true
+		} else {
+			return nil, false
+		}
 	} else {
 		fmt.Println("Failed to retrieve the page. Status code:", resp.StatusCode)
 		return nil, false
